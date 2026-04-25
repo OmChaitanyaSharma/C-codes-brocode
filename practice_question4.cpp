ec9f03bce3b840f2a4c00d30b2fd6f8992c32780
OmChaitanyaSharma <omranchi732@gmail.com>
2026-04-25 21:43:55 +0530

    commit 10

--- practice_question4.cpp
+++ practice_question4.cpp
@@ -4,3 +4,55 @@
 // // Display the original array.
 // // Sort the array using the Bubble Sort algorithm in ascending order.
 // // Display the sorted array.
+
+#include <iostream>
+
+void bubblesort(int array[], int size);
+
+int main()
+{
+    using std::cin;
+    using std::cout;
+    using std::string;
+
+    int array[10];
+    int size = sizeof(array) / sizeof(int);
+    for (int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+        cout << "\t";
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << '\n';
+    }
+
+    bubblesort(array, size);
+}
+
+void bubblesort(int array[], int size)
+{
+    using std::cin;
+    using std::cout;
+    using std::string;
+    int temp;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    cout << "\n"
+         << "the sorted algorithm is as follows\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << "\n";
+    }
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,24 +1,58 @@
-#include<iostream>
-double add(double prices[] , int size);
+// Question:// Write a C++ program to:
 
-int main(){
-    using std::cout;
+// // Take 10 integers as input from the user and store them in an array.
+// // Display the original array.
+// // Sort the array using the Bubble Sort algorithm in ascending order.
+// // Display the sorted array.
+
+#include <iostream>
+
+void bubblesort(int array[], int size);
+
+int main()
+{
     using std::cin;
+    using std::cout;
     using std::string;
 
-    double prices[]={49.99,12.32,123.78};
-    int size = sizeof(prices)/sizeof(double);
-    double total=add(prices , size);
-    cout<<"the total is "<< total;
-
-    return 0;
-}
-
-double add(double prices[] , int size){
-    double total=0;
-
-    for( double i: prices){
-        total +=prices;
+    int array[10];
+    int size = sizeof(array) / sizeof(int);
+    for (int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+        cout << "\t";
     }
 
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << '\n';
+    }
+
+    bubblesort(array, size);
+}
+
+void bubblesort(int array[], int size)
+{
+    using std::cin;
+    using std::cout;
+    using std::string;
+    int temp;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    cout << "\n"
+         << "the sorted algorithm is as follows\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << "\n";
+    }
 }
