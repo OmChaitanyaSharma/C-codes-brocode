e2450b8a6955db6f6c1509ad2c062ba4b93a1e49
OmChaitanyaSharma <omranchi732@gmail.com>
2026-04-26 13:16:55 +0530

    commit number i dont know but this is what i wish to commit

--- 36_2darray.cpp
+++ 36_2darray.cpp
@@ -10,15 +10,15 @@ int main()
                         {"mustang", "endevour", "fiat"},
                         {"mustang", "endevour", "fiat"}};
 
-    string foods[2][3];
+    int rows = sizeof(cars) / sizeof(cars[0]);       // size of whole/size of one row
+    int cols = sizeof(cars[0]) / sizeof(cars[0][0]); // size of one row/ size of one element
 
-    cout << cars[0][0] << " ";
-    cout << cars[0][1] << " ";
-    cout << cars[0][2] << "\n";
-    cout << cars[1][0] << " ";
-    cout << cars[1][1] << " ";
-    cout << cars[1][2] << "\n";
-    cout << cars[2][0] << " ";
-    cout << cars[2][1] << " ";
-    cout << cars[2][2] << "\n";
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << cars[i][j] << " ";
+        }
+        cout << '\n';
+    }
 }
