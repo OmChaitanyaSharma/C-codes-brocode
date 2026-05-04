6bc5588a7015d8c8449e91d2f46360546520cc46
OmChaitanyaSharma <omranchi732@gmail.com>
2026-05-05 03:32:46 +0530

    commit

---
+++ 38_memory_addresses.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main(){
+    using std::cout;
+    using std::cin;
+    using std::string;
+
+    int age = 32;
+    string name="om chaitanya sharma";
+    char word='3';
+    bool boolean=true;
+
+    cout<< &age  <<'\n';
+    cout<< &name  <<'\n';
+    cout<< &word <<'\n';
+    cout<< &boolean<<'\n';
+    cout<< name<<'\n';
+
+}
